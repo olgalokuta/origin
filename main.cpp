4495ff3bd8de774253cbf2adc0383fe218de0034
Olga Lokuta <olokuta20j@gmail.com>
2022-03-09 19:28:53 +0300

    Improved function fib()

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 int fib(int n) {
     int res;
-    if (n <= 2) res = 1;
+    if (n == 1) res = 0;
+    else if (n == 2 || n == 3) res = 1;
     else res = fib(n - 1) + fib(n - 2);
     return res; 
 }
